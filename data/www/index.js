05034a2a68a41a118580376031f675d58cb85c3d
iTecAI <matteovh@gmail.com>
2021-12-10 23:57:56 -0500

    main page

--- data/www/index.js
+++ data/www/index.js
@@ -10,6 +10,19 @@ var SKILLS = [
     "Raspberry Pi",
 ];
 
+function checkVisible(selector) {
+    var top_of_element = $(selector).offset().top;
+    var bottom_of_element =
+        $(selector).offset().top + $(selector).outerHeight();
+    var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
+    var top_of_screen = $(window).scrollTop();
+
+    return (
+        bottom_of_screen > top_of_element &&
+        top_of_screen < bottom_of_element - $(selector).outerHeight() * 0.25
+    );
+}
+
 function scrollHorizontallyCode(e) {
     // https://stackoverflow.com/questions/23952491/jquery-mousewheel-scroll-horizontally
     var delta = -Math.max(-1, Math.min(1, e.originalEvent.wheelDelta));
@@ -40,6 +53,19 @@ async function get_len_projects(username) {
     return length;
 }
 
+function updateNav() {
+    if (checkVisible(".whoami")) {
+        $(".section[data-section=whoami]").addClass("selected");
+        $(".section").not("[data-section=whoami]").removeClass("selected");
+    } else if (checkVisible(".projects")) {
+        $(".section[data-section=projects]").addClass("selected");
+        $(".section").not("[data-section=projects]").removeClass("selected");
+    } else if (checkVisible(".experience")) {
+        $(".section[data-section=experience]").addClass("selected");
+        $(".section").not("[data-section=experience]").removeClass("selected");
+    }
+}
+
 $(document).ready(function () {
     get_len_projects("itecai").then((l) => $("#count-projects").text(l));
     window.setInterval(function () {
@@ -49,4 +75,15 @@ $(document).ready(function () {
     }, 15000);
     $(".code-header").on("wheel", scrollHorizontallyCode);
     $(".personal").on("wheel", scrollHorizontallyCode);
+    $(".sections .section").on("click", function (e) {
+        document
+            .querySelector("." + $(e.delegateTarget).attr("data-section"))
+            .scrollIntoView({
+                behavior: "smooth",
+            });
+        $(".section.selected").removeClass("selected");
+        $(e.delegateTarget).addClass("selected");
+        setTimeout(updateNav, 300);
+    });
+    $(window).on("wheel", updateNav);
 });
